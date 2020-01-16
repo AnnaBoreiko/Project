ebad68fff03a189b38c266d3f534bd081eec0e7c
AnnaBoreiko <annboreikoo@gmail.com>
2020-01-16 18:09:05 +0000

    Вихідна версія проекту

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import reg, entry, exit, reset
+
+urlpatterns = [
+
+    path('reg', reg),
+    path('entry', entry),
+    path('exit', exit),
+    path('reset', reset)
+]
